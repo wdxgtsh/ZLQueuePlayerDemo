22a5c6d2a4bf88920a023a8fde53c143db2574cd
zhaolei <zhaolei@allinmd.cn>
2016-05-10 18:10:45 +0800

    add cover image

--- ZLPlayerDemo/ViewController.m
+++ ZLPlayerDemo/ViewController.m
@@ -23,46 +23,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.playerVC = [[ZLPlayerVC alloc] initView];
-    self.playerVC.view.frame = CGRectMake(0, 20, PHONE_WIDTH, PHONE_WIDTH * 9 / 16);
+    self.playerVC.view.frame = CGRectMake(0, 140, PHONE_WIDTH, PHONE_WIDTH * 9 / 16);
     [self.view addSubview:self.playerVC.view];
     [self.playerVC createAVPlayerWithTitleItem:nil andNormalItem:nil];
-
-    
-    
-//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(deviceOrientationDidChange) name:UIDeviceOrientationDidChangeNotification object:nil];
-    
-    // Do any additional setup after loading the view, typically from a nib.
 }
-
-#pragma mark |-- deviceOrientationDidChange
-//- (void)deviceOrientationDidChange{
-//
-//    UIDeviceOrientation orientation = [UIDevice currentDevice].orientation;
-//    switch (orientation) {
-//        case UIDeviceOrientationPortrait:
-//            self.playerVC.view.transform = CGAffineTransformMakeRotation(0);
-//            self.playerVC.view.frame = CGRectMake(0, 0, PHONE_WIDTH,PHONE_WIDTH * 6 / 9);
-//            break;
-//        case UIDeviceOrientationLandscapeLeft:
-//            self.playerVC.view.transform = CGAffineTransformMakeRotation(M_PI_2);
-//            self.playerVC.view.frame = CGRectMake(0, 0, PHONE_WIDTH,PHONE_HEIGHT);
-//            break;
-//            
-//        case UIDeviceOrientationLandscapeRight:
-//            self.playerVC.view.transform = CGAffineTransformMakeRotation(-M_PI_2);
-//            self.playerVC.view.frame = CGRectMake(0, 0, PHONE_WIDTH,PHONE_HEIGHT);
-//            break;
-//        default:
-//            break;
-//    }
-//    
-//    
-//    NSLog(@"self.view --- %zd --- > %@", orientation, NSStringFromCGRect(self.view.frame));
-//    NSLog(@"playervc --- %zd --- > %@", orientation, NSStringFromCGRect(self.playerVC.view.frame));
-//    NSLog(@"  %f    %f ", PHONE_WIDTH, PHONE_HEIGHT);
-//    
-//    
-//}
 
 
 - (void)didReceiveMemoryWarning {
--- ZLPlayerDemo/ZLPlayerVC.m
+++ ZLPlayerDemo/ZLPlayerVC.m
@@ -11,6 +11,7 @@
 #import "Common.h"
 #import "MyAVPlayerItem.h"
 #import "ZLPlayerView.h"
+#import <MediaPlayer/MediaPlayer.h>
 
 // 竖屏 bar的高度
 #define TopBarH_N 30
@@ -33,9 +34,28 @@
 // 当前时间与剩余时间的 lable的宽度
 #define TimeLabel_W 70
 
+//滑动的有效距离
+#define kValueUseful                5
+// 音量 +/-
+#define kVolumeStep                 0.02f
+// 亮度 +/-
+#define kBrightnessStep             0.02f
+//屏幕亮度View 的宽度
+#define kBrightnessViewWidth        125
+
+
+typedef NS_ENUM(NSInteger, GestureType){
+    GestureTypeOfNone = 0,
+    GestureTypeOfVolume,
+    GestureTypeOfBrightness,
+    GestureTypeOfProgress,
+};
+
 
 
 @interface ZLPlayerVC ()
+//手势类型
+@property (nonatomic,assign)GestureType gestureType;
 
 //topView  bottomView
 @property (nonatomic, strong) UIView * topView;
@@ -91,6 +111,18 @@
 //是否锁定
 @property (nonatomic, assign) BOOL isOriationLocked;
 
+//手势起点
+@property (nonatomic ,assign) CGPoint  startPoint;
+
+//是否全屏
+@property (nonatomic ,assign) BOOL isFullScreen;
+//. 屏幕亮度
+@property (nonatomic,strong) UIImageView * brightnessView;
+@property (nonatomic,strong) UIProgressView * brightnessProgress;
+
+
+@property (nonatomic, strong) UIImageView * coverImageView;
+
 @end
 
 @implementation ZLPlayerVC
@@ -99,18 +131,21 @@
 - (instancetype)initView{
     self = [super init];
     if (self) {
-        [self loadTopbar];
-        [self loadBottomBar];
-        [self loadCountDownLabel];
-        [self addNoti];
-        [self loadLockButton];
-    }
+            }
     return self;
 }
 
 - (void)viewDidLoad{
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blackColor];
+    [self loadTopbar];
+    [self loadBottomBar];
+    [self loadCountDownLabel];
+    [self addNoti];
+    [self loadLockButton];
+    [self loadBrightnessView];
+    [self loadCoverImageView];
+
 }
 
 
@@ -272,7 +307,7 @@
         UISlider * slider = [[UISlider alloc] init];
         slider.enabled = NO;
         [slider addObserver:self forKeyPath:@"value" options:NSKeyValueObservingOptionNew context:nil];
-        
+        slider.value = 0;
         UIGraphicsBeginImageContextWithOptions((CGSize){ 1, 1 }, NO, 0.0f);
         UIImage *transparentImage = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
@@ -284,6 +319,7 @@
         [slider addTarget:self action:@selector(scrubbingDidBegin) forControlEvents:UIControlEventTouchDown];
         [slider addTarget:self action:@selector(scrubbingDidChange) forControlEvents:UIControlEventValueChanged];
         [slider addTarget:self action:@selector(scrubbingDidEnd) forControlEvents:(UIControlEventTouchUpInside | UIControlEventTouchCancel)];
+        [slider addObserver:self forKeyPath:@"value" options:NSKeyValueObservingOptionNew context:nil];
         [self.bottomView addSubview:slider];
         [slider mas_makeConstraints:^(MASConstraintMaker *make) {
             make.leading.trailing.equalTo(self.videoProgressView);
@@ -328,6 +364,49 @@
     });
     [self.view bringSubviewToFront:self.lockButton];
 }
+- (void)loadBrightnessView{
+    _brightnessView = ({
+        UIImageView * imageView = [[UIImageView alloc] init];
+        imageView.image = [UIImage imageNamed:@"Video_brightness_bg.png"];
+        imageView.alpha = 0;
+        [self.view addSubview:imageView];
+        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerX.equalTo(self.view.mas_centerX);
+            make.centerY.equalTo(self.view.mas_centerY);
+        }];
+        
+        
+        imageView;
+    });
+    
+    _brightnessProgress = ({
+        UIProgressView * progressView = [[UIProgressView alloc] init];
+        progressView.trackImage = [UIImage imageNamed:@"Video_num_bg.png"];
+        progressView.progressImage = [UIImage imageNamed:@"Video_num_front.png"];
+        progressView.progress = [UIScreen mainScreen].brightness;
+        [_brightnessView addSubview:progressView];
+        [progressView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.centerX.equalTo(_brightnessView.mas_centerX);
+            make.width.equalTo(@80);
+            make.bottom.equalTo(_brightnessView.mas_bottom).offset(-20);
+            make.height.equalTo(@2);
+        }];
+        progressView;
+    });
+}
+- (void)loadCoverImageView{
+    self.coverImageView = ({
+        UIImageView * imageView = [[UIImageView alloc] init];
+        [self.view addSubview:imageView];
+        
+        [imageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.leading.trailing.width.height.equalTo(self.view);
+        }];
+        
+        imageView.backgroundColor = [UIColor redColor];
+        imageView;
+    });
+}
 
 #pragma mark |-- deviceOrientationDidChange
 - (void)deviceOrientationDidChange:(NSNotification *)noti{
@@ -343,6 +422,8 @@
     
     switch (orientation) {
         case UIDeviceOrientationPortrait:{
+            //是否全屏
+            _isFullScreen = NO;
             //隐藏锁定按钮
             self.lockButton.hidden = YES;
             //显示 bottomView
@@ -361,6 +442,7 @@
             
             break;
         case UIDeviceOrientationLandscapeLeft:{
+            _isFullScreen = YES;
             self.lockButton.hidden = NO;
             [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
             //显示 bottomView  和  topView
@@ -378,6 +460,7 @@
             break;
             
         case UIDeviceOrientationLandscapeRight:{
+            _isFullScreen = YES;
             self.lockButton.hidden = NO;
             [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
             //显示 bottomView  和  topView
@@ -401,22 +484,209 @@
 }
 
 #pragma mark |---- touchBegin  touchMoved  touchEnd
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    if(self.avQueuePlayer.currentItem == self.adPlayerItem) return;
-    if (self.bottomView.hidden) {
-        [self showTopAndBottomView];
-    }else{
-        [self hiddenTopAndBottomView];
+#pragma mark
+#pragma mark - touch event
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    _startPoint = CGPointZero;
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint currentLocation = [touch locationInView:self.view];
+    CGFloat offset_x = currentLocation.x - _startPoint.x;
+    CGFloat offset_y = currentLocation.y - _startPoint.y;
+    
+    if (CGPointEqualToPoint(_startPoint,CGPointZero))
+    {
+        _startPoint = currentLocation;
+        return;
+    }
+    _startPoint = currentLocation;
+    
+    if (_gestureType == GestureTypeOfNone)
+    {
+        // 横向 右侧 调整音量
+        if ((currentLocation.x > self.view.frame.size.width * 0.5) &&
+            (ABS(offset_x) <  ABS(offset_y)) &&
+            _isFullScreen == YES)
+        {
+            _gestureType = GestureTypeOfVolume;
+        }
+        // 横向 左侧 调整音量
+        else if ((currentLocation.x < self.view.frame.size.width*0.5) &&
+                 (ABS(offset_x) <= ABS(offset_y)) &&
+                 _isFullScreen == YES)
+        {
+            _gestureType = GestureTypeOfBrightness;
+        }
+        else if ((ABS(offset_x) > ABS(offset_y)))
+        {
+            _gestureType = GestureTypeOfProgress;
+        }
+    }
+    if ((_gestureType == GestureTypeOfProgress) && (ABS(offset_x) > ABS(offset_y)))
+    {
+        if(_videoSlider.enabled == NO)
+        {
+            return;
+        }
+        
+        
+        if (offset_x > 0)
+        {
+            // debugLog(@"横向向右");
+            _videoSlider.value += 0.005;
+        }
+        else
+        {
+            // debugLog(@"横向向左");
+            _videoSlider.value -= 0.005;
+        }
+        
+        NSLog(@"1----------> ", _videoSlider.value);
+    }
+    else if ((_gestureType == GestureTypeOfVolume) &&
+             (currentLocation.x > self.view.frame.size.width*0.5) &&
+             (ABS(offset_x) <= ABS(offset_y)) &&
+             _isFullScreen == YES)
+    {
+        
+        if (offset_y > kValueUseful )
+        {
+            [self volumeAdd:-kVolumeStep];
+        }
+        else if(offset_y < -kValueUseful)
+        {
+            [self volumeAdd:kVolumeStep];
+        }
+    }
+    else if ((_gestureType == GestureTypeOfBrightness) &&
+             (currentLocation.x < self.view.frame.size.width*0.5) &&
+             (ABS(offset_x) <= ABS(offset_y)) &&
+             _isFullScreen == YES)
+    {
+        if (offset_y > kValueUseful)
+        {
+            _brightnessView.alpha = 1;
+            [self brightnessAdd:-kBrightnessStep];
+        }
+        else if(offset_y < -kValueUseful)
+        {
+            _brightnessView.alpha = 1;
+            [self brightnessAdd:kBrightnessStep];
+        }
     }
 }
 
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    //    [NSObject cancelPreviousPerformRequestsWithTarget:self];//可以成功取消全部。
+    //    [[self class] cancelPreviousPerformRequestsWithTarget:self];//可以成功取消全部。
+    //    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(hidenControlBar) object:nil];
+    
+    UITouch * touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.view];
+    
+    if (_gestureType == GestureTypeOfNone &&
+        !CGRectContainsPoint(_bottomView.frame, point) &&
+        !CGRectContainsPoint(_topView.frame, point))
+    {
+        // 轻拍手势 隐藏/显示状态栏
+        [UIView animateWithDuration:1 animations:^{
+            if (self.bottomView.hidden) {
+                [self showTopAndBottomView];
+            }else{
+                [self hiddenTopAndBottomView];
+            }
+        }];
+    }
+    else if (_gestureType == GestureTypeOfProgress)
+    {
+        _gestureType = GestureTypeOfNone;
+        
+        if(_videoSlider.enabled == NO)
+        {
+            return;
+        }
+        
+//        if([self checkLocalVedioNoLog:_movieSlider] == NO)
+//        {
+//            return;
+//        }
+//        
+        [self sliderScrollingEnded];
+    }
+    else
+    {
+        _gestureType = GestureTypeOfNone;
+        
+        if (_brightnessView.alpha != 0)
+        {
+            [UIView animateWithDuration:0.3 animations:^{
+                _brightnessView.alpha = 0;
+            }];
+        }
+    }
+}
+
+
+// 拖动播放进度条
+-(void)sliderScrollingEnded
+{
+
+    
+    NSLog(@"value -----> %f", _videoSlider.value);
+    double currentTime = floor(_videoTotalTime *_videoSlider.value);
+    
+    NSLog(@"currentTime -----> %f", currentTime);
+    // 转换成CMTime才能给player来控制播放进度
+    CMTime dragedCMTime = CMTimeMake(currentTime, 1);
+
+
+    __weak typeof (self) _weakSelf = self;
+    [self.avQueuePlayer seekToTime:dragedCMTime completionHandler:
+     ^(BOOL finish){
+         if (finish) {
+             [_weakSelf.avQueuePlayer play];
+         }
+     }];
+}
+
+
+
+/// 声音改变.
+- (void)volumeAdd:(CGFloat)step
+{
+    [MPMusicPlayerController applicationMusicPlayer].volume += step;;
+}
+
+/// 屏幕亮度改变.
+- (void)brightnessAdd:(CGFloat)step
+{
+
+    
+    [UIScreen mainScreen].brightness += step;
+    NSLog(@" --------- %f ------- %f",  _brightnessProgress.progress, step);
+    
+    _brightnessProgress.progress = [UIScreen mainScreen].brightness;
+}
+
+
 #pragma mark |---- sliderAction
 - (void)scrubbingDidBegin{
-    NSLog(@"%s", __func__);
+    _gestureType = GestureTypeOfNone;
 }
 
 - (void)scrubbingDidChange{
-    NSLog(@"%s", __func__);
+    if (_videoSlider.enabled == NO) {
+        return;
+    }
+    
+    _gestureType = GestureTypeOfProgress;
+    
+
 }
 
 - (void)scrubbingDidEnd{
@@ -451,8 +721,6 @@
     [self.topView performSelector:@selector(setHidden:) withObject:[NSNumber numberWithBool:YES] afterDelay:ToolView_Show_Time];
     [self.bottomView performSelector:@selector(setHidden:) withObject:[NSNumber numberWithBool:YES] afterDelay:ToolView_Show_Time];
 }
-
-
 
 
 #pragma mark |---- load AVQueuePlayer
@@ -522,6 +790,8 @@
     
     [self.view bringSubviewToFront:self.countDownLabel];
     [self.view bringSubviewToFront:self.lockButton];
+    [self.view bringSubviewToFront:self.brightnessView];
+    [self.view bringSubviewToFront:self.coverImageView];
 }
 
 
@@ -534,7 +804,9 @@
     }else{
         self.isNormalVideoPlayer = NO;
         self.countDownLabel.text = @"";
-
+        [self.avQueuePlayer seekToTime:kCMTimeZero completionHandler:^(BOOL finished) {
+            [self pause];
+        }];
     }
 }
 
@@ -543,19 +815,28 @@
     self.playBtn.selected = YES;
 }
 
+- (void)pause{
+    if (self.playerView.queuePlayer) {
+        [self.playerView.queuePlayer pause];
+    }
+    self.playBtn.selected = NO;
+}
+
 #pragma mark |---- observeValueForKeyPath
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
     if ([keyPath isEqualToString:@"currentItem"]) {
-
+        AVQueuePlayer * queuePlayer = (AVQueuePlayer*)object;
+        if(queuePlayer.currentItem == self.normalPlayerItem){
+            _videoSlider.enabled = YES;
+            _videoTotalTime = (self.normalPlayerItem.asset.duration.value / self.normalPlayerItem.asset.duration.timescale);
+        }
     }else if([keyPath isEqualToString:@"status"]){
         MyAVPlayerItem *playerItem = (MyAVPlayerItem*)object;
         if (playerItem == self.adPlayerItem) {
             _videoTotalTime = (playerItem.asset.duration.value / playerItem.asset.duration.timescale);
-        }else if(playerItem == self.normalPlayerItem){
-            self.videoSlider.enabled = YES;
-            _videoTotalTime = (playerItem.asset.duration.value / playerItem.asset.duration.timescale);
         }
         if ([playerItem status] == AVPlayerStatusReadyToPlay) {
+//            self.coverImageView.hidden = YES;
             [self play];
             if (self.avQueuePlayer.currentItem == self.adPlayerItem) {
                 [self.adPlayerItem removeObserver:self forKeyPath:@"status"];
@@ -565,6 +846,17 @@
         float bufferTime = [self availableDuration];
         float durationTime = CMTimeGetSeconds([self.normalPlayerItem duration]);
         [self.videoProgressView setProgress:bufferTime / durationTime animated:YES];
+    }else if([keyPath isEqualToString:@"value"]){
+        UISlider * slider = (UISlider *)object;
+        
+//        double currentTime = floor(_videoTotalTime * slider.value);
+//        CMTime dragedCMTime = CMTimeMake(currentTime, 1);
+//        [_avQueuePlayer seekToTime:dragedCMTime completionHandler:^(BOOL finished) {
+//            if (finished) {
+//                [self play];
+//            }
+//        }];
+        
     }
 }
 
@@ -577,8 +869,9 @@
     [self.avQueuePlayer removeAllItems];
     
     //隐藏顶部底部工具栏
-    [self hiddenTopAndBottomView];
+//    [self hiddenTopAndBottomView];
     if(self.avQueuePlayer.items.count == 0){
+        self.countDownLabel.hidden = NO;
         [self createAVPlayerWithTitleItem:nil andNormalItem:nil];
     }
 }
@@ -590,6 +883,7 @@
         _isOriationLocked = YES;
     }else{
         _isOriationLocked = NO;
+        [self roationViewWithOrientation:[UIDevice currentDevice].orientation];
     }
 }
 
